aae81873f17e3a549c3829067545e03ffb4f051f
biplob kumar <biplobkumar125@gmail.com>
2023-03-01 19:26:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+let popup=document.getElementById("popup")
+
+
+function openPopup() {
+    popup.classList.add("open-popup")
+}
+
+function closepopup() {
+    popup.classList.remove("open-popup")
+}
